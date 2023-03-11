7215e0ec0fad25c8b3ad3e8a8d7067e4e1e207c2
iho21s <ginacabalse11@gmail.com>
2023-03-12 02:27:14 +0800

    commit all

---
+++ phrestaurant/README.md
@@ -0,0 +1,4 @@
+<<<<<<< HEAD
+=======
+phfood
+>>>>>>> b81ea7fd1bcbff10b6cc7ec2b5915f59b996ef20
---
+++ phrestaurant/frontend/src/branches/branches.vue
@@ -0,0 +1,28 @@
+<template>
+    <select v-model="selectedBranch" class="branch-select">
+      <option v-for="branch in branches" :key="branch.id" @click="redirectToBranch">{{ branch.name }}</option>
+    </select>
+  </template>
+<script>
+export default {
+  data() {
+    return {
+      branches: [
+        { id: 1, name: 'Inasal'    },
+        { id: 2, name: 'karenderia' },
+      ],
+      selectedBranch: 1
+    };
+  }
+};
+
+</script>
+  
+<style>
+ .branch-select {
+    font-size: 16px;
+    background-color: #fff;
+    border: none;
+    padding: 6px;
+ }   
+</style>
